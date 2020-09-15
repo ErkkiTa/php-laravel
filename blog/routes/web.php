ead72289a56337289395a8dbb297635f64e47b13
ErkkiTa <erkkitamb5@gmail.com>
2020-09-15 11:45:40 +0300

    uus töö

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+     $books = App\Models\Book::where('language', 'Estonian')->get();
+    // $books = DB::table('books')->where ('language', 'Estonian')->get();
+    return view('welcome',['books' => $books]);
+    
 });
